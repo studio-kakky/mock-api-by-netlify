2d317691c0be043226531589d73974a8f2e58eef
柿内暢昌 <nob@studio-kakky.com>
2019-12-20 22:32:48 +0900

    ルーティング実装

---
+++ apimock/src/endpoints-list.js
@@ -0,0 +1,3 @@
+module.exports = [
+  { method: 'GET', url: '/v1/some/endpoint/', file: 'endpoints/some/get/index.js' }
+];
--- apimock/src/index.js
+++ apimock/src/index.js
@@ -1,6 +1,29 @@
+const endpoints = require('endpoints-list');
+const fs = require('fs');
+
 exports.handler = async (events) => {
+  const path = events.path.replace(/^api\//, '');
+  const endpoint = endpoints.find(v => v.url === path);
+
+  if (!endpoint) {
+    return {
+      statusCode: 404,
+      body: `Endpoint Not Defined ${JSON.stringify(events)}`
+    }
+  }
+
+  try {
+    fs.statSync(endpoint.file)
+  } catch {
+    return {
+      statusCode: 404,
+      body: `Endpoint File Not Found ${JSON.stringify(events)}`
+    }
+  }
+
+  const endpointFunc = require(endpoint.file);
   return {
     statusCode: 200,
-    body: JSON.stringify(events),
+    body: JSON.stringify(endpointFunc(event)),
   }
 };
